1592a2820f7b05446c2201c46f128c5635ebdbbf
sora <sorazora1.4@gmail.com>
2023-08-25 19:48:45 -0700

    organized code and too high guess message

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 "use strict";
 
+// DOM Elements
+const messageBox = document.querySelector(".message");
+const guessBox = document.querySelector(".guess");
+
 // Generate a random number (1 - 20)
 const number = Math.trunc(Math.random() * 20);
 
-document.querySelector('.number').textContent = number
+document.querySelector(".number").textContent = number;
 
 // Check if the guessed number is the same as the random number generated
 document.querySelector(".check").addEventListener("click", () => {
-  const userInput = Number(document.querySelector(".guess").value);
+  const userInput = Number(guessBox.value);
 
   // Check if the user inputted a value
   if (!userInput) {
-    document.querySelector(".message").textContent = "Please Enter a Number!";
-    return
-  } 
-  
-  // Check if the user guessed the right number  
+    messageBox.textContent = "Please Enter a Number!";
+    return;
+  }
+
+  // Check if the user guessed the right number
   if (userInput === number) {
-    document.querySelector(".message").textContent = "You guessed the number!";
+    messageBox.textContent = "You guessed the number!";
+  }
+
+  // Check if the user guessed too high
+  if (userInput > number) {
+    messageBox.textContent = "Guessed too high!";
   }
 });
