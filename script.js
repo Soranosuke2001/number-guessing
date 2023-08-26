03b15d1b0a1fef6a5aab48aa8f696efdbc978f77
sora <sorazora1.4@gmail.com>
2023-08-25 20:17:27 -0700

    resets the game on button click

--- script.js
+++ script.js
@@ -4,15 +4,13 @@
 const messageBox = document.querySelector(".message");
 const guessBox = document.querySelector(".guess");
 const scoreBox = document.querySelector(".score");
-const randomNumberBox = document.querySelector('.number')
-const body = document.querySelector('body')
+const randomNumberBox = document.querySelector(".number");
+const body = document.querySelector("body");
 
 let userScore = 20;
 
 // Generate a random number (1 - 20)
-const number = Math.trunc(Math.random() * 20) + 1;
-
-randomNumberBox.textContent = number;
+let number = Math.trunc(Math.random() * 20) + 1;
 
 // Decrease the score by 1 for each wrong guess
 const scoreAdjust = () => {
@@ -34,10 +32,11 @@ document.querySelector(".check").addEventListener("click", () => {
   if (userInput === number) {
     messageBox.textContent = "You guessed the number!";
 
-    // Change the background color and increase randomNumberBox width
+    // Change the background color, increase randomNumberBox width and show the random number
     // NOTE: Adds the styling inline, does not change the CSS file
-    body.style.backgroundColor = '#60b347'
-    randomNumberBox.style.width = '30rem'
+    body.style.backgroundColor = "#60b347";
+    randomNumberBox.style.width = "30rem";
+    randomNumberBox.textContent = number;
   }
 
   // Check if the user guessed too high
@@ -56,4 +55,21 @@ document.querySelector(".check").addEventListener("click", () => {
   if (userScore <= 0) {
     messageBox.textContent = "You lost the game!";
   }
+});
+
+// Resetting the game when the "Again!" button is clicked
+document.querySelector(".again").addEventListener("click", () => {
+  // Reset Scores
+  userScore = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+
+  // Reset text and guess box
+  scoreBox.textContent = userScore;
+  messageBox.textContent = "Start guessing...";
+  guessBox.value = "";
+  randomNumberBox.textContent = "?";
+
+  // Reset styling
+  body.style.backgroundColor = "#222";
+  randomNumberBox.style.width = "15rem";
 });
