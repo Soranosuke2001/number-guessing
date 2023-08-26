fc2774c90b3dcbbeaec1fe47ef5d629fbc2bec68
sora <sorazora1.4@gmail.com>
2023-08-25 19:40:53 -0700

    generates a random number

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-'use strict';
+"use strict";
 
-// DOM Manipulation
+// Generate a random number (1 - 20)
+const number = Math.trunc(Math.random() * 20);
 
 // Check if the guessed number is the same as the random number generated
-document.querySelector('.check').addEventListener('click', () => {
-    const userInput = Number(document.querySelector('.guess').value)
+document.querySelector(".check").addEventListener("click", () => {
+  const userInput = Number(document.querySelector(".guess").value);
 
-    // Check if the user inputted a value
-    if (!userInput) {
-        document.querySelector('.message').textContent = 'Please Enter a Number!'
-    }
-})
+  // Check if the user inputted a value
+  if (!userInput) {
+    document.querySelector(".message").textContent = "Please Enter a Number!";
+  }
+});
