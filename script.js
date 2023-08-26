262c2847835fcd4f0793d7dff142a5b5fddba357
sora <sorazora1.4@gmail.com>
2023-08-25 19:44:46 -0700

    checks if the user guessed teh correct number

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Generate a random number (1 - 20)
 const number = Math.trunc(Math.random() * 20);
 
+document.querySelector('.number').textContent = number
+
 // Check if the guessed number is the same as the random number generated
 document.querySelector(".check").addEventListener("click", () => {
   const userInput = Number(document.querySelector(".guess").value);
@@ -10,5 +12,11 @@ document.querySelector(".check").addEventListener("click", () => {
   // Check if the user inputted a value
   if (!userInput) {
     document.querySelector(".message").textContent = "Please Enter a Number!";
+    return
+  } 
+  
+  // Check if the user guessed the right number  
+  if (userInput === number) {
+    document.querySelector(".message").textContent = "You guessed the number!";
   }
 });
