29a1b43cd83fc6b1c984c5e185a6835f4688ec66
sora <sorazora1.4@gmail.com>
2023-08-25 19:38:03 -0700

    check user input validity

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// DOM Manipulation
+
+// Check if the guessed number is the same as the random number generated
+document.querySelector('.check').addEventListener('click', () => {
+    const userInput = Number(document.querySelector('.guess').value)
+
+    // Check if the user inputted a value
+    if (!userInput) {
+        document.querySelector('.message').textContent = 'Please Enter a Number!'
+    }
+})
